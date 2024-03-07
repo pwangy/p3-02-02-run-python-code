cf3a1d817d9fbf53c2a1ada7b78bddbb79b4a885
pwangy <blipsandclicks@gmail.com>
2024-03-07 15:55:10 -0800

    complete lab

---
+++ app.py
@@ -0,0 +1,7 @@
+# we can run code by using python lib/app.py in the cli
+print('Herro world!', end='') # tells the interperter to not create a new line.
+print('Herro sun!', end='!! ')
+print('Herro sky!', end='!!!\n') # \n creates a new line
+print('Herro moon!')
+
+# if you're in cli using python, use 'exit()' or control+d to exit.
